43124a9c506bffd7c4bd2a41cf7fdab4a852b236
zhangshaolong <369669902@qq.com>
2019-04-01 23:05:13 +0800

    use cache for build

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function (content) {
   return (
-`module.exports = function (data) {
-  const Simplite = require('simplite');
-  const renderer = Function (Simplite.dataKey, Simplite.toCodeBlock(${JSON.stringify(content)}));
+`const Simplite = require('simplite');
+const renderer = Function (Simplite.dataKey, Simplite.toCodeBlock(${JSON.stringify(content)}));
+module.exports = function (data) {
   return renderer.call(Simplite, data);
 }`)
 }
